d8c418e5afa00e0fb78129f82b1b01e7cae92ffd
Max H. Balsmeier <mhbalsmeier@gmail.com>
2021-08-25 03:04:48 +0200

    formal changes

--- op_configs/server_0.sh
+++ op_configs/server_0.sh
@@ -1,11 +1,11 @@
 # This is the default operational run setup for the server. Remember
 
-opennwp_home_dir=~/opennwp # directory of EFS
+efs_home_dir=~/EFS # directory of EFS
 cycle=(0 6 12 18) # the UTC times of the analyses
 delta_t_between_analyses=21600 # the temporal distance between two analyses
 run_spans=($((168*3600)) $((168*3600)) $((168*3600)) $((168*3600))) # the lengths of the runs
 target=4080 # time length we target for the whole procedure
-ndvar_home_dir=~/ndvar # the directory where ndvar resides
+da_home_dir=~/GAME-DA # the directory where ndvar resides
 game_home_dir=~/GAME # the home directory of GAME
 ftp_destination=~/website/data # The FTP directory.
 run_script=run_model_0.sh # the run script template you want to use (must exist in the directory run_scripts)
@@ -22,16 +22,16 @@ figs_save_path=~/website/data # the path to which the maps will be saved
 omp_num_threads=2 # number of OMP threads
 
 # That's it, now the procedure will be started.
-source $opennwp_home_dir/sh/root_script.sh
+source $efs_home_dir/sh/root_script.sh
 
 # associated crontab:
 ## m h  dom mon dow   command
 #SHELL=/bin/bash
 #PYTHONPATH=/usr/bin
-#20 3 * * * /home/ubuntu/opennwp/op_configs/server_0.sh > /home/ubuntu/log_0Z 2>&1
-#20 9 * * * /home/ubuntu/opennwp/op_configs/server_0.sh > /home/ubuntu/log_6Z 2>&1
-#20 15 * * * /home/ubuntu/opennwp/op_configs/server_0.sh > /home/ubuntu/log_12Z 2>&1
-#20 21 * * * /home/ubuntu/opennwp/op_configs/server_0.sh > /home/ubuntu/log_18Z 2>&1
+#20 3 * * * /home/ubuntu/EFS/op_configs/server_0.sh > /home/ubuntu/log_0Z 2>&1
+#20 9 * * * /home/ubuntu/EFS/op_configs/server_0.sh > /home/ubuntu/log_6Z 2>&1
+#20 15 * * * /home/ubuntu/EFS/op_configs/server_0.sh > /home/ubuntu/log_12Z 2>&1
+#20 21 * * * /home/ubuntu/EFS/op_configs/server_0.sh > /home/ubuntu/log_18Z 2>&1
 #0 3 * * * /home/ubuntu/updating/update_procedure.sh > /home/ubuntu/log_updating 2>&1
 #19 3 * * * sudo service apache2 restart
 #
--- sh/root_script.sh
+++ sh/root_script.sh
@@ -4,11 +4,11 @@ START=$(date +%s)
 
 # This file manages model runs including pre- and postprocessing.
 
-source $opennwp_home_dir/sh/determine_latest_analysis_time.sh
+source $efs_home_dir/sh/determine_latest_analysis_time.sh
 
 # deleting old data from the model output directory
 directory=$ftp_destination/visualizations/$analysis_hour"UTC"
-source $opennwp_home_dir/sh/cleanup.sh
+source $efs_home_dir/sh/cleanup.sh
 
 # setting the run ID
 analysis_hour_extended_string=$analysis_hour
@@ -22,12 +22,12 @@ START_ASSIMILATION=$(date +%s)
 
 # executing the obs_collector
 echo "Starting to collect observational data ..."
-$ndvar_home_dir/obs_collector/run.sh $ndvar_home_dir $analysis_year $analysis_month $analysis_day $analysis_hour_extended_string
+$da_home_dir/obs_collector/run.sh $da_home_dir $analysis_year $analysis_month $analysis_day $analysis_hour_extended_string
 echo "Collection of observational data completed."
 
 # executing the formatter
 echo "Starting to format observational data ..."
-$ndvar_home_dir/formatter/run_formatter.sh $ndvar_home_dir $analysis_year $analysis_month $analysis_day $analysis_hour_extended_string
+$da_home_dir/formatter/run_formatter.sh $da_home_dir $analysis_year $analysis_month $analysis_day $analysis_hour_extended_string
 # executing ndvar
 grid_file_name=B5L26T${toa}_O${orography_id}_OL${orography_layers}_SCVT.nc
 
@@ -35,7 +35,7 @@ grid_file_name=B5L26T${toa}_O${orography_id}_OL${orography_layers}_SCVT.nc
 # default
 background_file=$game_home_dir/test_generator/test_states/test_2_$grid_file_name
 # background state file from the previous run
-source $opennwp_home_dir/sh/determine_previous_analysis_time.sh
+source $efs_home_dir/sh/determine_previous_analysis_time.sh
 analysis_hour_extended_string_prev=$analysis_hour_prev
 if [ $analysis_hour_prev -lt 10 ]
 then
@@ -48,16 +48,16 @@ then
 background_file=$background_file_candidate
 fi
 
-$ndvar_home_dir/run_ndvar.sh $omp_num_threads $toa $orography_layers $ndvar_home_dir $background_file $orography_id $analysis_year $analysis_month $analysis_day $analysis_hour_extended_string $game_home_dir
+$da_home_dir/run_ndvar.sh $omp_num_threads $toa $orography_layers $da_home_dir $background_file $orography_id $analysis_year $analysis_month $analysis_day $analysis_hour_extended_string $game_home_dir
 
 rm -r $game_home_dir/output/$run_id_previous
 # cleaning the input directory of ndvar
-rm $ndvar_home_dir/input/*
+rm $da_home_dir/input/*
 
 END_ASSIMILATION=$(date +%s)
 DIFF_ASSIMILATION=$(echo "$END_ASSIMILATION - $START_ASSIMILATION" | bc)
 
-cp $opennwp_home_dir/run_scripts/$run_script $game_home_dir/run_scripts/nwp.sh
+cp $efs_home_dir/run_scripts/$run_script $game_home_dir/run_scripts/nwp.sh
 # making the model input file executable
 chmod +x $game_home_dir/run_scripts/nwp.sh
 
@@ -69,11 +69,11 @@ DIFF_MODEL=$(echo "$END_MODEL - $START_MODEL" | bc)
 
 # clean-up of FTP directories
 directory=$ftp_destination/model_output/surface/$analysis_hour"UTC"
-source $opennwp_home_dir/sh/cleanup.sh
+source $efs_home_dir/sh/cleanup.sh
 directory=$ftp_destination/model_output/pressure_levels/$analysis_hour"UTC"
-source $opennwp_home_dir/sh/cleanup.sh
+source $efs_home_dir/sh/cleanup.sh
 directory=$ftp_destination/visualizations/$analysis_hour"UTC"
-source $opennwp_home_dir/sh/cleanup.sh
+source $efs_home_dir/sh/cleanup.sh
 
 # copying the output to the FTP server.
 cp $game_home_dir/output/$run_id/*surface.grb2 $ftp_destination/model_output/surface/$analysis_hour"UTC"/
@@ -86,10 +86,10 @@ then
 
 if [ $run_span -gt $((72*3600)) ]
 then
-$opennwp_home_dir/plot_scripts/maps_server_0.sh 0 $map_plot_interval_early $analysis_hour $analysis_day $analysis_month $analysis_year $figs_save_path/visualizations/$analysis_hour"UTC" $game_home_dir $run_id $((72*3600))
-$opennwp_home_dir/plot_scripts/maps_server_0.sh $((72*3600 + $map_plot_interval_late)) $map_plot_interval_late $analysis_hour $analysis_day $analysis_month $analysis_year $figs_save_path/visualizations/$analysis_hour"UTC" $game_home_dir $run_id $run_span
+$efs_home_dir/plot_scripts/maps_server_0.sh 0 $map_plot_interval_early $analysis_hour $analysis_day $analysis_month $analysis_year $figs_save_path/visualizations/$analysis_hour"UTC" $game_home_dir $run_id $((72*3600))
+$efs_home_dir/plot_scripts/maps_server_0.sh $((72*3600 + $map_plot_interval_late)) $map_plot_interval_late $analysis_hour $analysis_day $analysis_month $analysis_year $figs_save_path/visualizations/$analysis_hour"UTC" $game_home_dir $run_id $run_span
 else
-$opennwp_home_dir/plot_scripts/maps_server_0.sh 0 $map_plot_interval_early $analysis_hour $analysis_day $analysis_month $analysis_year $figs_save_path/visualizations/$analysis_hour"UTC" $game_home_dir $run_id $run_span
+$efs_home_dir/plot_scripts/maps_server_0.sh 0 $map_plot_interval_early $analysis_hour $analysis_day $analysis_month $analysis_year $figs_save_path/visualizations/$analysis_hour"UTC" $game_home_dir $run_id $run_span
 fi
 
 fi
