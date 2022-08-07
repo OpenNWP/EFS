836b6729798d13b43e94918d68dbddac1100ae7c
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-08-07 22:21:39 +0200

    formal change

--- op_configs/server_0.sh
+++ op_configs/server_0.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-efs_home_dir=~/code/EFS # directory of EFS
+efs_home_dir=~/EFS # directory of EFS
 cycle=(0 6 12 18) # the UTC times of the analyses
 delta_t_between_analyses_min=360 # the temporal distance between two analysesin minutes
 run_spans_min=($((168*60)) $((168*60)) $((168*60)) $((168*60))) # the lengths of the runs in minutes
 model_id=0 # 0: GAME, 1: L-GAME
 target=4200 # time length we target for the whole procedure
-real2game_home_dir=~/code/real2GAME # the directory where real2GAME resides
-model_home_dir=~/code/GAME # the home directory of the model
+real2game_home_dir=~/real2GAME # the directory where real2GAME resides
+model_home_dir=~/GAME # the home directory of the model
 ftp_destination=~/website/data # the directory where to place the output files
 analysis_delay_min=175 # the number of minutes after which an analysis becomes available
 plot_maps=1 # set this to one if you want to plot maps
