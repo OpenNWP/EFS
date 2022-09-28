248d10f0713f7f38f23375935d654f5da55df4db
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-09-28 22:21:26 +0200

    ICON-D2 props inluded

--- op_configs/l-game_in_icon-d2.sh
+++ op_configs/l-game_in_icon-d2.sh
@@ -5,14 +5,14 @@ model_src_id=3 # 1: ICON-Global, 2: GAME, 3: ICON-D2
 model_target_id=2 # 1: GAME, 2: L-GAME
 n_layers=26 #  number of layers of the model
 nsoillays=5 # number of soil layers of the model
-cycle=(0 6 12 18) # the UTC times of the analyses
+cycle=(0 3 6 9 12 15 18 21) # the UTC times of the analyses
 delta_t_between_analyses_min=360 # the temporal distance between two analysesin minutes
 run_spans_min=($((27*60)) $((27*60)) $((27*60)) $((27*60))) # the lengths of the runs in minutes
 target=4200 # time length we target for the whole procedure
 real2game_home_dir=~/real2GAME # the directory where real2GAME resides
 model_home_dir=~/GAME # the home directory of the model
 ftp_destination=~/website/data # the directory where to place the output files
-analysis_delay_min=175 # the number of minutes after which an analysis becomes available
+analysis_delay_min=70 # the number of minutes after which an analysis becomes available
 plot_maps=1 # set this to one if you want to plot maps
 map_plot_interval_early_min=$((1*60)) # the temporal distance between two plots before 72 hrs
 map_plot_interval_late_min=$((6*60)) # the temporal distance between two plots after 72 hrs
@@ -33,10 +33,11 @@ source $efs_home_dir/sh/root_script.sh
 ## m h  dom mon dow   command
 #SHELL=/bin/bash
 #PYTHONPATH=/usr/bin
-#20 3 * * * /home/ubuntu/EFS/op_configs/server_0.sh > /home/ubuntu/log_0Z 2>&1
-#20 9 * * * /home/ubuntu/EFS/op_configs/server_0.sh > /home/ubuntu/log_6Z 2>&1
-#20 15 * * * /home/ubuntu/EFS/op_configs/server_0.sh > /home/ubuntu/log_12Z 2>&1
-#20 21 * * * /home/ubuntu/EFS/op_configs/server_0.sh > /home/ubuntu/log_18Z 2>&1
+#20 3 * * * /home/ubuntu/EFS/op_configs/l-game_in_icon-d2.sh > /home/ubuntu/log_0Z 2>&1
+#20 9 * * * /home/ubuntu/EFS/op_configs/l-game_in_icon-d2.sh > /home/ubuntu/log_6Z 2>&1
+#20 15 * * * /home/ubuntu/EFS/op_configs/l-game_in_icon-d2.sh > /home/ubuntu/log_12Z 2>&1
+#20 21 * * * /home/ubuntu/EFS/op_configs/l-game_in_icon-d2.sh > /home/ubuntu/log_18Z 2>&1
+#50 2 * * * /home/ubuntu/updating/update_procedure.sh > /home/ubuntu/log_updating 2>&1
 #50 2 * * * /home/ubuntu/updating/update_procedure.sh > /home/ubuntu/log_updating 2>&1
 #
 
